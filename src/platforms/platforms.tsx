6e6f91fdd80024f8ccd2914d2c16db7fc1d085b0
Riccardo Consolandi <rc@capsula.app>
2022-07-03 20:18:15 +0200

    refactor: platforms; change file name in platforms
    	- changed info file name in platforms

--- src/platforms/platforms.tsx
+++ src/platforms/platforms.tsx
@@ -1,4 +1,5 @@
 import * as React from "react";
+import "./platforms.css";
 
 const Info = ({ game }: any) => {
   return (
@@ -57,7 +58,9 @@ const Info = ({ game }: any) => {
                 style={{ width: 15, marginLeft: 5 }}
                 viewBox="0 0 576 512"
               >
-                <path d="M528 0h-480C21.5 0 0 21.5 0 48v320C0 394.5 21.5 416 48 416h192L224 464H152C138.8 464 128 474.8 128 488S138.8 512 152 512h272c13.25 0 24-10.75 24-24s-10.75-24-24-24H352L336 416h192c26.5 0 48-21.5 48-48v-320C576 21.5 554.5 0 528 0zM512 288H64V64h448V288z" />
+                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512">
+                  <path d="M0 93.7l183.6-25.3v177.4H0V93.7zm0 324.6l183.6 25.3V268.4H0v149.9zm203.8 28L448 480V268.4H203.8v177.9zm0-380.6v180.1H448V32L203.8 65.7z" />
+                </svg>
               </svg>
             );
           case "Linux":
