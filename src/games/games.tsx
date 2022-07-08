a39d9991db198907ad3d6d0ed643f58228336cd8
Riccardo Consolandi <rconsolandi@gmail.com>
2022-07-08 17:11:38 +0200

    refactor: games.tsx; sort games from the latest in the page

--- src/games/games.tsx
+++ src/games/games.tsx
@@ -1,9 +1,10 @@
-import * as React from "react";
 import { useState } from "react";
 import { GameApi } from "../API/api";
+import { Images } from "../images/images";
+import { mapping } from "./models";
+import Platforms from "../platforms/platforms";
 import { Infos } from "../infos/infos";
 import "./games.css";
-import Platforms from "../platforms/platforms";
 
 export const Games = () => {
   const [open, setOpen] = useState("");
@@ -27,19 +28,23 @@ export const Games = () => {
 
   return (
     <div className="container">
-      {data.results?.map((game: any) => {
-        return (
+      {data.results
+        ?.sort(
+          (a: any, b: any) =>
+            b.released.substring(0, 4) - a.released.substring(0, 4)
+        )
+        .map((game: mapping) => (
           <div
             key={game.id}
             style={{ backgroundColor: `#${game.dominant_color}`, width: 300 }}
             className="items"
             onMouseOver={() => openInfo(game.id)}
           >
-            <img
-              src={game.background_image}
-              alt={game.name}
-              style={{ width: 300, borderRadius: ".5rem .5rem 0rem 0rem" }}
-            ></img>
+            <Images
+              background={game.background_image}
+              name={game.name}
+              screenshots={game.short_screenshots}
+            />
             <div className="infos">
               <Platforms game={game} />
               <p className="name">{game.name}</p>
@@ -48,8 +53,7 @@ export const Games = () => {
               <Infos open={open} game={game} />
             </div>
           </div>
-        );
-      })}
+        ))}
     </div>
   );
 };
